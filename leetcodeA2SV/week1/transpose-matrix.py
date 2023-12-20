0be2864df14ea033a6bf3c7f2cfb1391428e0e1e
Bisratasaye12 <138536048+Bisratasaye12@users.noreply.github.com>
2023-12-20 15:29:29 +0300

    Added solution for Transpose Matrix

---
+++ leetcodeA2SV/week1/transpose-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+       res = []
+       for col in range(len(matrix[0])):
+           r = []
+           for row in range(len(matrix)):
+               r.append(matrix[row][col])
+
+           res.append(r)
+       return res
