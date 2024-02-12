f912e25ece34ad95a21d93bea64d3ccc77b530f4
Bisratasaye12 <138536048+Bisratasaye12@users.noreply.github.com>
2024-02-12 15:11:23 +0300

    Add solution for Reverse Linked List

--- leetcodeA2SV/DSA/leetcode/reverse-linked-list.py
+++ leetcodeA2SV/DSA/leetcode/reverse-linked-list.py
@@ -6,16 +6,18 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        def rev(node, h=head):
-            if node == None:
-                return 
-            if node.next == None:
-                h = node
-                return h
-            h = rev(node.next)
-            node.next.next = node
-            node.next = None
-            return h
-        head = rev(head)
-        return head
-    
+        tail = head
+        if not head: return head
+        while tail.next:
+            tail = tail.next
+
+        prev, curr, n = None, head, head.next
+
+        while curr:
+            curr.next = prev
+            prev = curr
+            curr = n
+            if n:
+                n = n.next
+
+        return tail
